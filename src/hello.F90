91ff004347db80e0ac89f3cd8a3a9befcb6818a0
Andrew <anki0301@colorado.edu>
2023-06-06 13:56:44 -0600

    Added subroutines and functions to fizzbuzz

--- src/hello.F90
+++ src/hello.F90
@@ -6,7 +6,48 @@
 !> This is a Fortran program.
 !>
 !> @author Andrew King, @date 6/5/23
-program Hello
+program hello
     implicit none
-    print *, "Hello, World!"
-end program Hello
+    integer :: MAX_COUNT
+    parameter(MAX_COUNT = 15)
+  
+    print *, 'Hello world!'
+    call fizzbuzz(MAX_COUNT)
+end program hello
+  
+  subroutine fizzbuzz(max_count)
+    implicit none
+    integer, intent(in) :: max_count
+    logical :: fizzy, buzzy
+    integer :: i
+    
+    do i = 1, MAX_COUNT
+       print *, i
+       if (fizzy(i) .eqv. .true.) print *, 'Fizz'
+       if (buzzy(i) .eqv. .true.) print *, 'Buzz'
+    end do
+  end subroutine fizzbuzz
+  
+  function fizzy(number)
+    implicit none
+    integer, intent(in) :: number
+    logical fizzy
+  
+    if (mod(number, 3) .eq. 0) then
+       fizzy = .true.
+    else
+       fizzy = .false.
+    endif
+  end function fizzy
+  
+  function buzzy(number)
+    implicit none
+    integer, intent(in) :: number
+    logical buzzy
+  
+    if (mod(number, 5) .eq. 0) then
+       buzzy = .true.
+    else
+       buzzy = .false.
+    endif
+  end function buzzy
